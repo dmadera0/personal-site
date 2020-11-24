4a3f3aaa251f76c1b9914a8cecb52857c3bc2a0e
Daniel <dmadera0@gmail.com>
2020-11-23 19:59:39 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Daniels <code>src/App.js</code> Personal Website - in progress.
         </p>
         <a
           className="App-link"
