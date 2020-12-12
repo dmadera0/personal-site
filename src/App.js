c16e695f17d8a3edb10f3d5141ad032e282adcc6
Daniel <dmadera0@gmail.com>
2020-12-11 18:45:50 -0700

    added navbuttons and sandwhich

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'; 
 import './App.css';
-import {BrowserRouter as Router, } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import NavbarBrand from 'react-bootstrap/NavbarBrand'
 
 class App extends React.Component {
@@ -34,8 +35,17 @@ class App extends React.Component {
       <Router>
         <Container className="p-0" fluid ={true}>
 
-          <Navbar className = "border-bottom">
+          <Navbar className = "border-bottom" bg ="transparent" expand ='lg'>
             <Navbar.Brand>Daniels Website</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id = "navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+
           </Navbar>
         </Container>
 
