a49b4270b56c28d1ddd982df602a7c3cc7cf6bec
Daniel <dmadera0@gmail.com>
2020-12-18 19:32:28 -0700

    structure of about me

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Jumbo from '../components/Jumbo'
+import Content from '../components/Content'
 
 function AboutPage(props){
 
     return(
-        <p>All the cool shit that i do!</p>
+        <div>
+            <Jumbo title={props.title}/>
+
+            <Content>
+                <p>Im A Cold Blooded Killer</p>
+                <p>150lbs of twisted steel and sex apeal</p>
+                <p>Im dope and I do dope thing</p>
+            </Content>
+        </div>
     )
 
 }
