bac9fcbdef214d5fa51d16e1247222567f4256b1
Daniel <dmadera0@gmail.com>
2020-12-21 10:22:26 -0700

    form added

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -21,7 +21,22 @@ class ContactPage extends React.Component{
                 <Jumbo title={this.props.title} />
             <Content>
                 <Form>
-
+                    <Form.Group>
+                        <Form.Label htmlFor="full-name">Full Name</Form.Label>
+                        <Form.Control id="full-name" name="name" type= "text" value={this.state.name} onChange={this.handleChange}/>
+                    </Form.Group>
+                </Form>
+                <Form>
+                    <Form.Group>
+                        <Form.Label htmlFor="email">Email</Form.Label>
+                        <Form.Control id="email" name="email" type= "email" value={this.state.email} onChange={this.handleChange}/>
+                    </Form.Group>
+                </Form>
+                <Form>
+                    <Form.Group>
+                        <Form.Label htmlFor="message">Message</Form.Label>
+                        <Form.Control id="message" name="message" as= "textarea"rows="3" value={this.state.message} onChange={this.handleChange}/>
+                    </Form.Group>
                 </Form>
 
             </Content>
