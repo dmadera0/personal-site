3fce7c5fbf84c55198a6a48144de3a16aa88544c
Daniel <dmadera0@gmail.com>
2020-12-29 16:18:43 -0700

    contactpage

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,6 +3,7 @@ import Jumbo from '../components/Jumbo'
 import Content from '../components/Content'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Axios from 'axios';
 class ContactPage extends React.Component{
 
     constructor(props){
@@ -35,6 +36,29 @@ class ContactPage extends React.Component{
         this.setState({
             disabled: true
         })
+
+        Axios.post('http://localhost:2020/api/email', this.state)
+            .then(res => {
+                if(res.data.success) {
+                    this.setState({
+                        disabled: false,
+                        emailSent: true
+                    });
+                } else {
+                    this.setState({
+                        disabled: false,
+                        emailSent: false
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+
+                this.setState({
+                    disabled: false,
+                    emailSent: false
+                });
+            })
     }
     render(){
         return(
@@ -46,19 +70,17 @@ class ContactPage extends React.Component{
                         <Form.Label htmlFor="full-name">Full Name</Form.Label>
                         <Form.Control id="full-name" name="name" type= "text" value={this.state.name} onChange={this.handleChange}/>
                     </Form.Group>
-                </Form>
-                <Form>
+            
+                
                     <Form.Group>
                         <Form.Label htmlFor="email">Email</Form.Label>
                         <Form.Control id="email" name="email" type= "email" value={this.state.email} onChange={this.handleChange}/>
                     </Form.Group>
-                </Form>
-                <Form>
+                
                     <Form.Group>
                         <Form.Label htmlFor="message">Message</Form.Label>
                         <Form.Control id="message" name="message" as= "textarea"rows="3" value={this.state.message} onChange={this.handleChange}/>
                     </Form.Group>
-                </Form>
 
                 <Button className="d-inline-block" variant='primary' type='submit' disabled={this.state.disabled}>
                 Send
@@ -67,9 +89,8 @@ class ContactPage extends React.Component{
 
                 {this.state.emailSent === true && <p className='d-inline success-msg'>Email Sent</p>}
                 {this.state.emailSent === false && <p className='d-inline error-msg'>Email Not Sent</p>}
+                </Form>
             </Content>
-
-
             </div>
 
 
